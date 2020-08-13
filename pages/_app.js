01109a05ac30490703bf5c0e2a4f6ccc020f793b
octaviolage <octavio.lage@hotmail.com>
2020-08-13 01:12:20 -0300

    Pagina Inicial apresenta cards ao invés de lista

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import App from 'next/app';
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,90 @@
 import React from 'react';
 import Link from 'next/link';
+import styled from 'styled-components';
 
-export async function getStaticProps() {
+const Card = styled.div`
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 40%;
+  max-width: 200px;
+  border-radius: 5px;
+  background-color: #2a74ba;
+  display: inline-block;
+  margin: 1%;
+
+  :hover {
+  box-shadow: 0 12px 24px 0 rgba(0,0,0,0.4);
+  
+}
+`;
+const Container = styled.div`
+  background-color: rgba(0,0,0,0.3);
+  border-radius: 0px 0px 5px 5px;
+  text-align-last: center;
+`;
+
+const Image = styled.img`
+  width: 100%;
+`;
+
+const Type = styled.div`
+  background-color: #fff;
+  border-radius: 15px;
+  width: 40%;
+  display: inline-block;
+  margin: 0% 5% 5% 5%;
+`;
+
+const Name = styled.h4`
+  color: white;
+  margin: 0% 0% 3% 0%;
+  padding-top: 2%;
+`;
+
+export async function getPokemons() {
   const pokemons = await fetch('https://pokeapi.co/api/v2/pokedex/2/')
     .then((respostaDoServer) => {
       if (respostaDoServer.ok) {
         return respostaDoServer.json();
       }
 
-      throw new Error('Deu problema');
+      throw new Error('Deu problema na pokedex');
     })
     .then((respostaEmObjeto) => respostaEmObjeto.pokemon_entries);
+  return pokemons;
+}
 
+export async function getStaticProps() {
+  const lista = await getPokemons();
+  const pokemons = await lista.map(async (pokemon) => {
+    const pokemonJson = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.entry_number}/`)
+      .then((respostaDoServer) => {
+        if (respostaDoServer.ok) {
+          return respostaDoServer.json();
+        }
+
+        throw new Error('Erro na captura de pokemons');
+      });
+    return pokemonJson;
+  });
+  const pokedex = await (async () => {
+    const resultado = await Promise.all(pokemons);
+    return resultado;
+  })();
   return {
     props: {
-      pokemons,
+      pokedex,
     },
   };
 }
 
 export default function Home(props) {
-  const { pokemons } = props;
+  const { pokedex } = props;
 
   return (
     <div>
-      Pokédex - DevSoutinho - Da um like :)
+      Pokédex - DevSoutinho - Da um like :) <br />
+      >Branch  - octaviolage
       <ul>
         <li>
           <Link href="/">
@@ -38,17 +98,19 @@ export default function Home(props) {
         </li>
       </ul>
 
-      <ul>
-        {pokemons.map((pokemon) => (
-          <li key={pokemon.entry_number}>
-            <Link href={`/pokemon/${pokemon.entry_number}`}>
-              <a>
-                {pokemon.pokemon_species.name}
-              </a>
+        {pokedex.map((pokemon) => (
+            <Link href={`/pokemon/${pokemon.id}`}>
+              <Card>
+                <Image src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} />
+                <Container className="container">
+                  <Name>{`#${pokemon.id} ${pokemon.name.slice(0, 1).toUpperCase()}${pokemon.name.slice(1)}`}</Name>
+                  {pokemon.types.map((types) => (
+                    <Type>{types.type.name}</Type>
+                  ))}
+                </Container>
+              </Card>
             </Link>
-          </li>
         ))}
-      </ul>
     </div>
   );
 }
