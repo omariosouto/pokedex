b3c12e6a27f05c59e0fd93573c39d396db34df7b
Mario Souto <soutomarios@gmail.com>
2021-01-04 18:30:43 -0300

    update

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -1,30 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function Pokemon({ pokemon }) {
-  console.log();
   return (
     <div>
+      <h1>
+        {pokemon.name}
+      </h1>
       <img src={pokemon.sprites.front_default} alt="Imagem de um pokémon" />
     </div>
   );
 }
 
+Pokemon.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string,
+    sprites: PropTypes.shape({
+      front_default: PropTypes.string,
+    }),
+  }).isRequired,
+};
+
 export async function getStaticProps({ params }) {
   const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
-  .then((respostaDoServer) => {
-    if (respostaDoServer.ok) {
-      return respostaDoServer.json();
-    }
+    .then((respostaDoServer) => {
+      if (respostaDoServer.ok) {
+        return respostaDoServer.json();
+      }
 
-    throw new Error('Deu problema');
-  })
-  .then((respostaEmObjeto) => respostaEmObjeto);
+      throw new Error('Deu problema');
+    })
+    .then((respostaEmObjeto) => respostaEmObjeto);
 
   return {
     props: {
-      pokemon
-    }
-  }
+      pokemon,
+    },
+  };
 }
 
 export async function getStaticPaths() {
