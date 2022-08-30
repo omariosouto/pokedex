729e1c85ab31ae3224bfb90af247ca3204dc9000
Matheus Pazinati <matheuspazinati@gmail.com>
2022-08-30 05:27:23 -0300

    Altera estilo da pagina principal

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 function GlobalStyle() { //Estilo global - reset css
   return (
     <style global jsx>{`
+    @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap');
   * {
       box-sizing: border-box;
       margin: 0;
       padding: 0;
       text-decoration: none;
+      font-family: 'Lato', sans-serif;
+      list-style: none;
   }
 
     `}</style>
--- pages/index.js
+++ pages/index.js
@@ -23,27 +23,47 @@ export default function Home(props) {
   const { pokemons } = props;
 
   return (
-    <div>
-      Pokédex - DevSoutinho - Da um like :)
-      <ul>
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/sobre">
-            <a>Sobre o projeto</a>
-          </Link>
-        </li>
-      </ul>
+    <div style={{
+      background: 'linear-gradient(to bottom, #6AB7F5, #FFF)'
+    }}>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center'
+      }}>
+      <img
+         src="../images/logo.png"
+         alt="Logo do site"
+        />
+      </div>
+      <p style={{
+        textAlign: 'center',
+        fontSize: '25px',
+        margin: '30px 0 10px 0',
+        fontWeight: '700'
+      }}
+      >
+        Clique em um Pokémon para visualizá-lo na Pokédex.
+      </p>
 
-      <ul>
+      <ul style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '80px',
+        padding: '30px 8px'
+      }}>
         {pokemons.map((pokemon) => (
           <li key={pokemon.entry_number}>
             <Link href={`/pokemon/${pokemon.entry_number}`}>
               <a>
-                {pokemon.pokemon_species.name}
+                <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.entry_number}.png`}
+                alt={pokemon.pokemon_species.name}
+                style={{
+                  width: '130px'
+                }}
+                />
               </a>
             </Link>
           </li>
